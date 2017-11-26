a3738f827d56e0313d629471bb2729a453cbae70
Guilherme Mendes <guilherme8787@gmail.com>
2017-11-26 21:46:41 -0200

    Editar 2.1

--- src/connplayer/Editar.java
+++ src/connplayer/Editar.java
@@ -5,6 +5,13 @@
  */
 package connplayer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Orion
